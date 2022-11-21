1bb9fa500beb23c6f9e91a554e4c0b8c85638e00
KimiNoUso <35688791+TheKimiNoUso@users.noreply.github.com>
2022-11-21 19:05:28 +0000

    Add chat confirmation

--- src/main/java/com/kiminouso/kimivanish/VanishListeners.java
+++ src/main/java/com/kiminouso/kimivanish/VanishListeners.java
@@ -1,35 +1,39 @@
 package com.kiminouso.kimivanish;
 
 import com.kiminouso.kimivanish.listeners.VanishStatusUpdateEvent;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.checkerframework.checker.units.qual.C;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
 public class VanishListeners implements Listener {
     private final Set<UUID> recentlyUnhidden = new HashSet<>(); // Keeps recently unvanished players for 5 seconds.
+    private final Map<UUID, String> pendingMessages = new HashMap<>();
 
     @EventHandler
     private void onVanish(VanishStatusUpdateEvent event) {
-        if (KimiVanish.getPlugin(KimiVanish.class).getConfig().getBoolean("settings.vanish.protect-recently-unvanished")) {
-            if (recentlyUnhidden.contains(event.getPlayer().getUniqueId()))
-                return;
+        if (!KimiVanish.getPlugin(KimiVanish.class).getConfig().getBoolean("settings.vanish.protect-recently-unvanished"))
+            return;
 
-            if (event.isVanished())
-                return;
+        if (recentlyUnhidden.contains(event.getPlayer().getUniqueId()))
+            return;
 
-            recentlyUnhidden.add(event.getPlayer().getUniqueId());
-            Bukkit.getScheduler().runTaskLater(KimiVanish.getPlugin(KimiVanish.class), () -> recentlyUnhidden.remove(event.getPlayer().getUniqueId()), 5 * 20L);
-        }
+        if (event.isVanished())
+            return;
+
+        recentlyUnhidden.add(event.getPlayer().getUniqueId());
+        Bukkit.getScheduler().runTaskLater(KimiVanish.getPlugin(KimiVanish.class), () -> recentlyUnhidden.remove(event.getPlayer().getUniqueId()), 5 * 20L);
     }
 
     @EventHandler
@@ -67,36 +71,58 @@ public class VanishListeners implements Listener {
 
     @EventHandler
     private void onJoinMessage(PlayerJoinEvent event) {
-        if (event.getPlayer().hasPermission("kimivanish.hide")
-        && KimiVanish.getPlugin(KimiVanish.class).getConfig().getBoolean("settings.vanish.hide-connection-message")) {
+        if (!KimiVanish.getPlugin(KimiVanish.class).getConfig().getBoolean("settings.vanish.hide-connection-message"))
+            return;
+
+        if (event.getPlayer().hasPermission("kimivanish.hide"))
             event.setJoinMessage("");
-        }
     }
 
     @EventHandler
     private void onLeaveMessage(PlayerQuitEvent event) {
-        if (event.getPlayer().hasPermission("kimivanish.hide")
-                && KimiVanish.getPlugin(KimiVanish.class).getConfig().getBoolean("settings.vanish.hide-connection-message")) {
+        if (!KimiVanish.getPlugin(KimiVanish.class).getConfig().getBoolean("settings.vanish.hide-connection-message"))
+            return;
+
+        if (event.getPlayer().hasPermission("kimivanish.hide"))
             event.setQuitMessage("");
-        }
     }
 
     @EventHandler
     private void onKickMessage(PlayerKickEvent event) {
-        if (event.getPlayer().hasPermission("kimivanish.hide")
-                && KimiVanish.getPlugin(KimiVanish.class).getConfig().getBoolean("settings.vanish.hide-connection-message")) {
+        if (!KimiVanish.getPlugin(KimiVanish.class).getConfig().getBoolean("settings.vanish.hide-connection-message"))
+            return;
+
+        if (event.getPlayer().hasPermission("kimivanish.hide"))
             event.setLeaveMessage("");
-        }
     }
 
     @EventHandler
     private void onVanishHunger(FoodLevelChangeEvent event) {
+        if (!KimiVanish.getPlugin(KimiVanish.class).getConfig().getBoolean("settings.vanish.prevent-hunger-loss"))
+            return;
+
         if (!(event.getEntity() instanceof Player player))
             return;
 
-        if (KimiVanish.getPlugin(KimiVanish.class).getVanishManager().currentlyVanished.contains(player.getUniqueId())
-                && KimiVanish.getPlugin(KimiVanish.class).getConfig().getBoolean("settings.vanish.prevent-hunger-loss")) {
+        if (KimiVanish.getPlugin(KimiVanish.class).getVanishManager().currentlyVanished.contains(player.getUniqueId()))
             event.setCancelled(true);
+    }
+
+    @EventHandler
+    private void onVanishChat(AsyncPlayerChatEvent event) {
+        if (!KimiVanish.getPlugin(KimiVanish.class).getVanishManager().currentlyVanished.contains(event.getPlayer().getUniqueId()))
+            return;
+
+        Player player = event.getPlayer();
+
+        if (pendingMessages.containsKey(player.getUniqueId()) && event.getMessage().equals(pendingMessages.get(player.getUniqueId()))) {
+            pendingMessages.remove(player.getUniqueId());
+        } else {
+            event.setCancelled(true);
+            pendingMessages.remove(player.getUniqueId());
+            pendingMessages.put(player.getUniqueId(), event.getMessage());
+            TextComponent message = new TextComponent(ConfigUtils.getMessage("messages.vanish.confirm-message", player, event.getMessage()));
+            player.spigot().sendMessage(message);
         }
     }
 }
